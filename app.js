803b67e6fc8a52376c4fc326e10be97de489d0a9
Johana Rios <johana.rios@dualbootpartners.com>
2021-08-12 23:33:29 -0300

    setgunda edicion de la app

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('HOLA MUNDO Joha!'));
+app.get('/', (req, res) => res.send('HOLA MUNDO Nuevamente Joha!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
